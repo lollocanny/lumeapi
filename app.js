be46589c0c52875322bc1451ab7b54002c300ae0
lollo <lo.cannella7@gmail.com>
2021-06-30 12:02:56 +0200

    no message

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const userRoutes = require('./routes/utente');
+const requestRoutes = require('./routes/richiesta');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methhos', 'PUT, POST, PATCH, DELETE, GET');
+		return res.status(200).json({});
+	}
+	next();
+});
+
+//Routes which should handle request
+app.use('utente', userRoutes);
+app.use('/richiesta', requestRoutes);
+
+app.use((req, res, next) => {
+	const error = new Error('Non trovato');
+	error.status = 404;
+	next(error);
+})
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message
+		}
+	});
+});
+
+
+module.exports = app;
